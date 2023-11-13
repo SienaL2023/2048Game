35b107e2694436d5381713f95836586f4b1bfad6
SienaL2023 <xiaosiena@gmail.com>
2023-11-12 18:30:32 -0800

    updated logic.py with merge, compress, update and move left

--- logic.py
+++ logic.py
@@ -17,7 +17,78 @@ def start_game():
 def add_new_2(mat):
     r = random.randint(0,3)
     c = random.randint(0,3)
-    print("about to add a 2")
-    print("testing this to be pushed")
-    # check if empty before assigning num
+
+    while(mat[r][c] != 0):
+        r = random.randint(0, 3)
+        c = random.randint(0, 3)
     mat[r][c] = 2
+
+
+def get_current_state(mat):
+    # check if game is won or not
+    for i in range(4):
+        for j in range(4):
+            if(mat[i][j] == 2048):
+                return 'WON'
+            if(mat[i][j] == 0):
+                return 'GAME NOT OVER'
+    # no cell is empty,
+    # but any moves in all directions
+    # cells can merge and create empty cell
+    for i in range(3): # checks just three rows
+        for j in range(3): # checks just four columns
+            if (mat[i][j] == mat[i][j + 1] or mat[i][j] == mat[i+1][j]):
+                return 'GAME NOT OVER'
+    # checking last row
+    for j in range(3):
+        if(mat[3][j] == mat[3][j+1]):
+            return 'GAME NOT OVER'
+    # checking last column
+    for i in range(3):
+        if (mat[i][3] == mat[i+1][3]):
+            return 'GAME NOT OVER'
+    return 'LOST' # when none of the conditions r met
+
+# function to compress the grid, after every step before and after merging cells
+def compress(mat):
+    changed = False
+    new_mat = []
+    for i in range(4):
+        new_mat.append([0] * 4)
+
+    # loop to traverse through the rows
+    for i in range(4):
+        pos = 0
+        for j in range(4):
+            if(mat[i][j] != 0):
+                new_mat[i][pos] = mat[i][j]
+                if(j != pos):
+                    changed = True
+                pos += 1
+    return new_mat, changed
+
+
+# function to merge the cells in matrix before compressing
+def merge(mat): # this just does the math not the movement
+    changed = False
+    for i in range(4): # all the numbers bc four colmuns
+        for j in range(3): # 3 bc theres a plus one so u get the one next to it
+            # checks if elemnts r same and not zero
+            if(mat [i][j] == mat[i][j+1] and mat[i][j] != 0):
+                mat[i][j] = mat[i][j] * 2
+                mat[i][j+1] = 0 # one becomes double while one becomes zero
+                changed = True
+    return mat, changed
+
+# function to update matrix if we move left
+def move_left(mat):
+    # first compress the mat
+    new_mat, changed1 = compress(mat)
+    # merge the cells
+    new_mat, changed2 = merge(new_mat)
+
+    changed = changed1 or changed2
+    # compress again after merging
+    new_mat, temp = compress(new_mat)
+
+    return new_mat, changed
--- main.py
+++ main.py
@@ -2,7 +2,18 @@
 
 # 4 x 4 grid with directions to move um down left or right
 import logic
+# creates mat, starts game
 mat = logic.start_game()
 for x in mat:
+    print(x)
+
+print("")
+# moves left
+mat2, temp = logic.move_left(mat)
+# adds 2
+logic.add_new_2(mat2)
+# moves the 2 to the left
+mat2, temp = logic.move_left(mat2)
+for x in mat2:
     print(x)
 print("this is main!")
